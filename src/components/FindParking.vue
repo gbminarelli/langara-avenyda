2a54eb1f78dd805c256ed754acb9fa61284d3878
Gianlucci Badiali Minarelli <gianlucci.minarelli@gmail.com>
2019-03-17 20:30:56 -0700

    Create MarkerWindow

--- src/components/FindParking.vue
+++ src/components/FindParking.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="map">
-    <div class="searchParking">
-      <!-- <h2>Find a parking spot in Canada on Avenyda {{storedMarkers.data[0].latitud}} {{storedMarkers.data[0].longitud}}</h2> -->
+    <!-- <div class="searchParking">
+      <h2>Find a parking spot in Canada on Avenyda {{storedMarkers.data[0].latitud}} {{storedMarkers.data[0].longitud}}</h2>
       <h2 v-for="marker in storedMarkers" :key="marker.id">
         Marker with ID = {{marker.id}}: {{marker.latitud}}, {{marker.longitud}}
       </h2>
@@ -9,235 +9,274 @@
         <gmap-autocomplete
           @place_changed="setPlace">
         </gmap-autocomplete>
-        <button @click="addMarker">Add</button>
+        <button @click="">Add</button>
       </label>
       <br/>
 
-    </div>
+    </div> -->
     <br>
     <gmap-map
       :center="center"
-      :zoom="12"
+      :zoom="18"
       v-bind:options="mapStyle"
-      style="width:100%;  height: 70vh; margin-top:-5vh;"
-    >
+      style="width:100%;  height: 70vh; margin-top:-5vh;">
+
+      <gmap-info-window :options="infoOptions" :position="infoWindowPos" :opened="infoWinOpen" @closeclick="infoWinOpen=false">
+        <MarkerWindow :price="infoPrice" />
+        <!-- <div class="">
+          {{infoPrice}}
+        </div> -->
+      </gmap-info-window>
+
       <gmap-marker
         :key="index"
         v-for="(m, index) in markers"
         :position="m.position"
         :icon="{ url: require('../assets/pin.svg')}"
-        @click="center=m.position"
+        :clickable="true"
+        :price="m.price"
+        @click="toggleInfoWindow(m,index)"
       ></gmap-marker>
     </gmap-map>
   </div>
 </template>
 
 <script>
+import MarkerWindow from '@/components/MarkerWindow'
 const axios = require('axios');
 export default {
   name: "FindParking",
+  components: {
+    MarkerWindow
+  },
   data() {
     return {
-        storedMarkers: [],
-        mapStyle: {
+      storedMarkers: [],
+      mapStyle: {
         styles: [
-          {
-            featureType: "all",
-            elementType: "labels.text.fill",
-            styles: [
-              {
-                saturation: 10
-              },
-              {
-                color: "#d7ddd7"
-              },
-              {
-                lightness: 40
-              }
-            ]
-          },
-          {
-            featureType: "all",
-            elementType: "labels.text.stroke",
-            stylers: [
-              {
-                visibility: "on"
-              },
-              {
-                color: "#d7ddd7"
-              },
-              {
-                lightness: 10
-              }
-            ]
-          },
-          {
-            featureType: "all",
-            elementType: "labels.icon",
-            stylers: [
-              {
-                visibility: "off"
-              }
-            ]
-          },
-          {
-            featureType: "administrative",
-            elementType: "geometry.fill",
-            stylers: [
-              {
-                color: "#8fc18b"
-              },
-              {
-                lightness: 20
-              }
-            ]
-          },
-          {
-            featureType: "administrative",
-            elementType: "geometry.stroke",
-            stylers: [
-              {
-                color: "#8fc18b"
-              },
-              {
-                lightness: 17
-              },
-              {
-                weight: 1
-              }
-            ]
-          },
-          {
-            featureType: "landscape",
-            elementType: "geometry",
-            stylers: [
-              {
-                color: "#f9f9f4"
-              },
-              {
-                lightness: 20
-              }
-            ]
-          },
-          {
-            featureType: "poi",
-            elementType: "geometry",
-            stylers: [
-              {
-                color: "#8fc18b"
-              },
-              {
-                lightness: 21
-              }
-            ]
-          },
-          {
-            featureType: "road.highway",
-            elementType: "geometry.fill",
-            stylers: [
-              {
-                color: "#d7ddd7"
-              },
-              {
-                lightness: 17
-              }
-            ]
-          },
-          {
-            featureType: "road.highway",
-            elementType: "geometry.stroke",
-            stylers: [
-              {
-                color: "#d7ddd7"
-              },
-              {
-                lightness: 10
-              },
-              {
-                weight: .1
-              }
-            ]
-          },
-          {
-            featureType: "road.arterial",
-            elementType: "geometry",
-            stylers: [
-              {
-                color: "#d7ddd7"
-              },
-              {
-                lightness: 18
-              }
-            ]
-          },
-          {
-            featureType: "road.local",
-            elementType: "geometry",
-            stylers: [
-              {
-                color: "#d7ddd7"
-              },
-              {
-                lightness: 16
-              }
-            ]
-          },
-          {
-            featureType: "transit",
-            elementType: "geometry",
-            stylers: [
-              {
-                color: "#ebe3cd"
-              },
-              {
-                lightness: 19
-              }
-            ]
-          },
-          {
-            featureType: "water",
-            elementType: "geometry",
-            stylers: [
-              {
-                color: "#7ac0e2"
-              },
-              {
-                lightness: 17
-              }
-            ]
-          }
-        ]
+        {
+          featureType: "all",
+          elementType: "labels.text.fill",
+          styles: [
+            {
+              saturation: 10
+            },
+            {
+              color: "#d7ddd7"
+            },
+            {
+              lightness: 40
+            }
+          ]
+        },
+        {
+          featureType: "all",
+          elementType: "labels.text.stroke",
+          stylers: [
+            {
+              visibility: "on"
+            },
+            {
+              color: "#d7ddd7"
+            },
+            {
+              lightness: 10
+            }
+          ]
+        },
+        {
+          featureType: "all",
+          elementType: "labels.icon",
+          stylers: [
+            {
+              visibility: "off"
+            }
+          ]
+        },
+        {
+          featureType: "administrative",
+          elementType: "geometry.fill",
+          stylers: [
+            {
+              color: "#8fc18b"
+            },
+            {
+              lightness: 20
+            }
+          ]
+        },
+        {
+          featureType: "administrative",
+          elementType: "geometry.stroke",
+          stylers: [
+            {
+              color: "#8fc18b"
+            },
+            {
+              lightness: 17
+            },
+            {
+              weight: 1
+            }
+          ]
+        },
+        {
+          featureType: "landscape",
+          elementType: "geometry",
+          stylers: [
+            {
+              color: "#f9f9f4"
+            },
+            {
+              lightness: 20
+            }
+          ]
+        },
+        {
+          featureType: "poi",
+          elementType: "geometry",
+          stylers: [
+            {
+              color: "#8fc18b"
+            },
+            {
+              lightness: 21
+            }
+          ]
+        },
+        {
+          featureType: "road.highway",
+          elementType: "geometry.fill",
+          stylers: [
+            {
+              color: "#d7ddd7"
+            },
+            {
+              lightness: 17
+            }
+          ]
+        },
+        {
+          featureType: "road.highway",
+          elementType: "geometry.stroke",
+          stylers: [
+            {
+              color: "#d7ddd7"
+            },
+            {
+              lightness: 10
+            },
+            {
+              weight: .1
+            }
+          ]
+        },
+        {
+          featureType: "road.arterial",
+          elementType: "geometry",
+          stylers: [
+            {
+              color: "#d7ddd7"
+            },
+            {
+              lightness: 18
+            }
+          ]
+        },
+        {
+          featureType: "road.local",
+          elementType: "geometry",
+          stylers: [
+            {
+              color: "#d7ddd7"
+            },
+            {
+              lightness: 16
+            }
+          ]
+        },
+        {
+          featureType: "transit",
+          elementType: "geometry",
+          stylers: [
+            {
+              color: "#ebe3cd"
+            },
+            {
+              lightness: 19
+            }
+          ]
+        },
+        {
+          featureType: "water",
+          elementType: "geometry",
+          stylers: [
+            {
+              color: "#7ac0e2"
+            },
+            {
+              lightness: 17
+            }
+          ]
+        }
+      ]
       },
-
-
-      // default to montreal to keep it simple
-      // change this to whatever makes sense
-      center: { lat: 49.252, lng: -123.045 },
+      center: { lat: 49.2519, lng: -123.043 },
       places: [],
-      currentPlace: null
-
+      currentPlace: null,
+      infoPrice: null,
+      infoWindowPos: null,
+      infoWinOpen: false,
+      currentMidx: null,
+      infoOptions: {
+        pixelOffset: {
+          width: 0,
+          height: -60
+        }
+      }
     };
   },
 
   mounted() {
-    this.geolocate();
+    // Removing geolocation for the presentation.
+    // this.geolocate();
   },
 
   computed: {
-    // markers: [{position:{lat: 49.2519, lng: -123.043 }}]
     markers: function() {
       return this.storedMarkers.map(e => {
         return {
           position: {
             lat: e.latitud,
             lng: e.longitud
-          }
+          },
+          // label: {
+          //   color: 'white',
+          //   fontWeight: 'bold',
+          //   text: 'Hello world',
+          // },
+          price: Math.floor(Math.random() * (500 - 100) + 100) / 100
+          // price: Math.floor(Math.random()*(3)+1)
+          // price: Math.floor(Math.random()*(max-min+1)+min)
+          // infoText: `Marker at position: ${e.latitud}, ${e.longitud}`,
+          // label: 'A'
         }
       });
     }
   },
 
   methods: {
+    toggleInfoWindow: function(marker, idx) {
+      this.infoWindowPos = marker.position;
+      this.infoPrice = marker.price;
+      if (this.currentMidx == idx) {
+        this.infoWinOpen = !this.infoWinOpen;
+      }
+      else {
+        this.infoWinOpen = true;
+        this.currentMidx = idx;
+      }
+    },
     setPlace(place) {
       this.currentPlace = place;
     },
@@ -263,6 +302,7 @@ export default {
     }
   },
   created(){
+    // console.log(this.center);
      axios
         .get('https://wmdd-get-w5-1542jkb8k.now.sh/api/get/parkingSpot')
         .then(response => {
@@ -270,7 +310,7 @@ export default {
 
         })
         .catch(error => {
-          console.log(error)
+          console.error(error)
         })
   }
 };
